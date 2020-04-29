0ade2312142f78d06ec69cc8511da11c66302538
RodroPaq <58706760+RodroPaq@users.noreply.github.com>
2020-04-29 14:51:24 -0300

    3

---
+++ precache-manifest.55bacdcb626a9ed0ce8790761b9a187b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8227c937e5dac73aebefa7c586d77024",
+    "url": "/RodroPaq/menupedidosindex.html"
+  },
+  {
+    "revision": "9735b572bf8997d2143a",
+    "url": "/RodroPaq/menupedidosstatic/css/main.6a934dfc.chunk.css"
+  },
+  {
+    "revision": "87ae145aef9c9ab4124e",
+    "url": "/RodroPaq/menupedidosstatic/js/2.a199cc0d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RodroPaq/menupedidosstatic/js/2.a199cc0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9735b572bf8997d2143a",
+    "url": "/RodroPaq/menupedidosstatic/js/main.b2cda8a6.chunk.js"
+  },
+  {
+    "revision": "a798f67a4041e925127a",
+    "url": "/RodroPaq/menupedidosstatic/js/runtime-main.c9bf74a2.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8c0035ab8c02923b005d25fb619a89d0.js"
+  "/RodroPaq/menupedidosprecache-manifest.55bacdcb626a9ed0ce8790761b9a187b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/RodroPaq/menupedidosindex.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
