763d091d7f5c36d7b7383b9e0077487c11964b33
RodroPaq <58706760+RodroPaq@users.noreply.github.com>
2020-04-29 02:33:23 -0300

    Prueba 1

---
+++ precache-manifest.8c0035ab8c02923b005d25fb619a89d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "260a212a54f925108bb4c3efe0e4b64c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "61f4ee2c4758ace6e1d5",
+    "url": "/static/css/main.6a934dfc.chunk.css"
+  },
+  {
+    "revision": "2a90cbc26eccaf0fd82f",
+    "url": "/static/js/2.8b3127b2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8b3127b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61f4ee2c4758ace6e1d5",
+    "url": "/static/js/main.f98c5b09.chunk.js"
+  },
+  {
+    "revision": "a446c6f4295f76fbecee",
+    "url": "/static/js/runtime-main.e85ee0d1.js"
+  }
+]);
